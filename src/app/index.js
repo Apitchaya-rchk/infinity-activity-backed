d93d4e195e47bc9409f91e0625178c77e389cd3a
Apitchaya Radchakit <apitchaya.rchk@gmail.com>
2022-04-20 23:31:34 +0700

    add Access-Control-Allow-Origin

--- src/app/index.js
+++ src/app/index.js
@@ -20,12 +20,18 @@ if (config.isVercel) {
 
 app.use(bodyParser.json());
 
-app.use(
-    cors({
-      origin: '*',
-      optionsSuccessStatus: 200,
-    })
-  );
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// app.use(
+//     cors({
+//       origin: '*',
+//       optionsSuccessStatus: 200,
+//     })
+//   );
 
 app.use('/records', recordRouter);
 
