6c5889ae413dd8bdd6ddb96c6ec3ffb11e71e23a
Apitchaya Radchakit <apitchaya.rchk@gmail.com>
2022-04-20 21:52:14 +0700

    setting for Vercel

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+const config = require('../config');
+const PORT = config.port;
+
+const recordRouter = require('../routes/record.js');
+
+const app = express(); 
+
+if (config.isVercel) {
+    app.use(async (req, res, next) => {
+      await mongoose.connect(config.mongoUri, config.mongoOptions);
+      return next();
+    });
+  }
+
+app.use(bodyParser.json());
+
+app.use(
+    cors({
+      origin: '*',
+      optionsSuccessStatus: 200,
+    })
+  );
+
+app.use('/records', recordRouter);
+
+module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,11 +1,38 @@
+require('dotenv').config();
 
+// module.exports = {
+//     mongoUri: 'mongodb+srv://sandbox.nzcot.mongodb.net',
+//     mongoOptions: {
+//         user: 'm001-student',
+//         pass: 'm001-mongodb-basics',
+//         dbName: 'records',
+//         retryWrites: true,
+//         w: 'majority',
+//     },
+//     port: process.env.PORT || 4000
+// };
+
+
+// module.exports = {
+//     mongoUri: 'mongodb+srv://sandbox.nzcot.mongodb.net',
+//     mongoOptions: {
+//         user: 'm001-student',
+//         pass: 'm001-mongodb-basics',
+//         dbName: 'records',
+//         retryWrites: true,
+//         w: 'majority',
+//     },
+//     port: process.env.PORT || 4000
+//   };
 module.exports = {
-    mongoUri: 'mongodb+srv://sandbox.nzcot.mongodb.net',
+    isVercel: process.env.IS_VERCEL || false,
+    port: process.env.PORT || 4000,
+    mongoUri: process.env.MONGO_URI,
     mongoOptions: {
-        user: 'm001-student',
-        pass: 'm001-mongodb-basics',
-        dbName: 'records', 
-        retryWrites: true,
-        w: 'majority',
-    }
-};
+      user: process.env.MONGO_USER,
+      pass: process.env.MONGO_PASSWORD,
+      dbName: process.env.MONGO_DATABASE,
+      retryWrites: true,
+      w: 'majority',
+    },
+  };
